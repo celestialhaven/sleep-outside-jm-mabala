b9a2ca61028bca5c1fcc8c521da29b265561fd3b
celestialhaven <jonzmabz04@gmail.com>
2026-05-28 22:00:18 +0800

    new header added

---
+++ src/js/product-listing.js
@@ -0,0 +1,56 @@
+import Alert from "./Alert.js";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { getLocalStorage } from "./utils.mjs";
+
+const alert = new Alert();
+
+alert.render();
+
+const dataSource =
+  new ProductData("tents");
+
+const listElement =
+  document.querySelector(
+    ".product-list"
+  );
+
+const myList = new ProductList(
+  "tents",
+  dataSource,
+  listElement
+);
+
+function updateCartCount() {
+
+  const cartItems =
+    getLocalStorage("so-cart") || [];
+
+  const count =
+    cartItems.length;
+
+  const cartCount =
+    document.querySelector(
+      ".cart-count"
+    );
+
+  if (!cartCount) return;
+
+  if (count > 0) {
+
+    cartCount.textContent =
+      count;
+
+    cartCount.style.display =
+      "flex";
+
+  } else {
+
+    cartCount.style.display =
+      "none";
+  }
+}
+
+updateCartCount();
+
+myList.init();
